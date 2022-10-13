8d7d3321048023faf9f7c492fc21e438d55d4745
kcx064_LAB-PC <kcx064@163.com>
2022-10-14 00:38:53 +0800

    添加了V3的软件仿真

--- Control/H250_LOEUnknown_400HzParam.m
+++ Control/H250_LOEUnknown_400HzParam.m
@@ -7,10 +7,10 @@ MY_TAU_P_P_YAW = Pixhawk_CSC.Parameter( {single(0.1), 'MY_TAU_P_P_YAW'}  );
 MY_TAU_P_A_YAW = Pixhawk_CSC.Parameter( {single(0), 'MY_TAU_P_A_YAW'}  );
 
 MY_RATE_P = Pixhawk_CSC.Parameter( {single(14), 'MY_RATE_P'}  );
-MY_RATE_P_YAW = Pixhawk_CSC.Parameter( {single(0.0), 'MY_RATE_P_YAW'}  );
+MY_RATE_P_YAW = Pixhawk_CSC.Parameter( {single(1), 'MY_RATE_P_YAW'}  );
 
 MY_ATT_P = Pixhawk_CSC.Parameter( {single(3), 'MY_ATT_P'}  );
-MY_ATT_P_YAW = Pixhawk_CSC.Parameter( {single(0.0), 'MY_ATT_P_YAW'}  );
+MY_ATT_P_YAW = Pixhawk_CSC.Parameter( {single(0), 'MY_ATT_P_YAW'}  );
 
 MY_POS_P = Pixhawk_CSC.Parameter( {single(0.1), 'MY_POS_P'}  );
 MY_VEL_P = Pixhawk_CSC.Parameter( {single(0.15), 'MY_VEL_P'}  );
@@ -19,7 +19,7 @@ MY_VEL_ALT = Pixhawk_CSC.Parameter( {single(1), 'MY_VEL_ALT'}  );
 
 MY_SAT_AD = Pixhawk_CSC.Parameter({single(0.13), 'MY_SAT_AD'} );
 
-MY_K_CP = Pixhawk_CSC.Parameter({single(0.6), 'MY_K_CP'} );
+MY_K_CP = Pixhawk_CSC.Parameter({single(-1.4), 'MY_K_CP'} );
 
 Ts = 1/400;%1/400
 % 虽然理论推导显示，Tmotor=Tmotorbar=Tkd
